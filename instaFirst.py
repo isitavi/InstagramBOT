9d12fa454da900d1dd1564176e3baf5abd408720
isitavi <avijitdasnoyon@gmail.com>
2019-03-10 15:13:39 +0600

    Tryin to click on Not Now to stop notification popup

--- instaFirst.py
+++ instaFirst.py
@@ -11,8 +11,10 @@ class Instagram:
         self.driver = webdriver.Chrome("./chromedriver")
         self.url = "https://www.instagram.com/accounts/login/"
         self.driver.get(self.url)
-
+        sleep(3)
         self.logIn()
+        self.popupNotify()
+        input('stop')
         sleep(3)
         self.driver.close()
 
@@ -26,6 +28,17 @@ class Instagram:
             "//input[@name='password'][@type='password']")
         passwordField.send_keys(self.userpassword)
         loginBtn.click()
+
+    def popupNotify(self):
+        try:
+            sleep(2)
+            notnowBtn = self.driver.find_element_by_xpath(
+                "//button[contains('Not Now')]")
+            # HoLwm
+            notnowBtn.click()
+            sleep(2)
+        except Exception:
+            pass
 
 
 if __name__ == "__main__":
